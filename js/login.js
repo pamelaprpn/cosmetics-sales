95c27e9f6a1d9586fd2e38bba3d33e6071aa3603
Pamela Raiane <pamelaprpn@gmail.com>
2024-05-31 21:58:00 -0300

    feat: finish layout login and responsive

--- js/login.js
+++ js/login.js
@@ -1,19 +1,77 @@
-const form = document.querySelector('.form')
+
+function validadeEmail() {  
+    document.getElementById('email').addEventListener('blur', function(){
+        
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const email = document.getElementById("email").value;
+
+        if(!emailPattern.test(email)){
+            document.getElementById('emailError').style.display = 'block';
+            this.style.border = '1px solid red';
+            document.getElementById('emailError').textContent = "Por favor, digite um e-mail válido.";    
+        }else{
+            this.style.border = 'none';
+            document.getElementById('emailError').style.display = 'none';
+        }
+    });
+   
+}
+
+function validadeSenha() {
+    document.getElementById('password').addEventListener('blur', function(){
+
+        const password = document.getElementById("password").value;
+        const passwordPattern = /^(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+    
+        if(!passwordPattern.test(password)){
+            document.getElementById('passwordError').style.display = 'block';
+            this.style.border = '1px solid red'
+            document.getElementById('passwordError').textContent = 'Campo deve conter no minimo 8 caracteres, entre letras maiúsculas, minúsculas, numeros e caracteres especiais.'
+        }else{
+            
+        }
+    });
+}
 
 
-document.querySelector('.btn_login').addEventListener("submit", function(event){
-    let email = document.getElementById('email').value;
-    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(email)) {
-        alert("Por favor, Digite um e-mail válido.");
-        event.preventDefault();
-    }
+validadeEmail();
+validadeSenha();
 
-    let password = document.getElementById("password").value;
-    if (password.length < 8) {
-        alert("La contraseña debe tener al menos 8 caracteres.");
-        event.preventDefault();
-    }
-})
+
+
+
+
+// function validarForm (){
+//     validadeEmail();
+//     validadeSenha();
+
+//     document.querySelector('.btn_login').addEventListener('click', function(event){
+//         if(email.trim() === "" && password.trim() === ""){
+//             event.preventDefault();
+//             email.style.border = '1px solid red';
+//             password.style.border = '1px solid red';
+
+//             alert('teste')
+//         }
+//     })
+// }
+
+// validarForm();
+
+// document.getElementsByClassName('.btn_login').addEventListener("click", function(event){
+
+//     if(email.trim() === "" && password.trim() === ""){
+//         email.style.border = '1px solid red';
+//         document.getElementById('emailError').textContent = "Por favor, digite um e-mail válido."; 
+//         password.style.border = '1px solid red';
+//         document.getElementById('passwordError').textContent = 'Campo deve conter no minimo 8 caracteres, entre letras maiúsculas, minúsculas, numeros e caracteres especiais.'
+//         event.preventDefault();
+//     }
+    
+
+// });
+
+
+
 
 
