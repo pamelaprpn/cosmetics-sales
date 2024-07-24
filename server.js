4a1f6c94057706b9f21daa7cdadec4c4123d2619
Pamela Raiane <pamelaprpn@gmail.com>
2024-07-24 09:41:48 -0300

    feat: create servidor node.js

---
+++ server.js
@@ -0,0 +1,16 @@
+import http from "http";
+
+const PORT = 3000;
+
+const rotas = {
+    "/": "Curso de Express"
+};
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end(rotas[req.url])
+});
+
+server.listen(PORT, () => {
+    console.log("Servidor executando");
+});
